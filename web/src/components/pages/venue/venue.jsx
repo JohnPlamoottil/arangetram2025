c7a2a4b731a807f7715cc68689767a4176d19e38
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-06-09 14:22:46 -0500

    add google api into Venue

--- web/src/components/pages/venue/venue.jsx
+++ web/src/components/pages/venue/venue.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Navigation from "../../navigation-links/navigation-links";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+
+const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
 
 const Venue = () => {
   return (
     <div>
-      This is the venue page
       <Navigation />;
+      <APIProvider apiKey={apiKey}>
+        <Map
+          style={{ width: "500px", height: "500px" }}
+          zoom={15}
+          center={{ lat: 42.354492334658644, lng: -88.01122761349113 }}
+        />
+      </APIProvider>
     </div>
   );
 };
