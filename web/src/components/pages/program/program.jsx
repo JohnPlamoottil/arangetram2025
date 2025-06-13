7c4795a98c6650caa6571544b4f70353679f0a92
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-06-13 16:37:54 -0500

    start to plan out program page

--- web/src/components/pages/program/program.jsx
+++ web/src/components/pages/program/program.jsx
@@ -4,7 +4,12 @@ import Navigation from "../../navigation-links/navigation-links";
 const Program = () => {
   return (
     <div>
-      <Navigation />;
+      <Navigation />
+      <div className="program__container">
+        <h1>Program</h1>
+        <div className="program_1">Program 1</div>
+        <div className="program_2">Program 2</div>
+      </div>
     </div>
   );
 };
