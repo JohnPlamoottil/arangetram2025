0c65f6af8dead0a79259fd0a17855b82fb4ab135
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-06-13 09:29:36 -0500

    add marker to google map

--- web/src/components/pages/venue/venue.jsx
+++ web/src/components/pages/venue/venue.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navigation from "../../navigation-links/navigation-links";
 import "./venue.css";
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 import Footer from "../../footer/footer";
 
 const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
@@ -17,6 +17,15 @@ const Venue = () => {
             zoom={15}
             center={{ lat: 42.354492334658644, lng: -88.01122761349113 }}
           />
+          <Map
+            style={{ width: "1048px", height: "768px" }}
+            zoom={15}
+            center={{ lat: 42.354492334658644, lng: -88.01122761349113 }}
+          >
+            <Marker
+              position={{ lat: 42.354492334658644, lng: -88.01122761349113 }}
+            />
+          </Map>
         </APIProvider>
       </div>
       <Footer />
