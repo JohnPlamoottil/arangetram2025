2a980f9057f6a20544147b39badc256852e7de11
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-06-13 16:40:18 -0500

    replace tillana image for green varnum corner image for the sliding animation from right

--- web/src/components/pages/home/slidingImage.jsx
+++ web/src/components/pages/home/slidingImage.jsx
@@ -1,10 +1,10 @@
-import "./SlidingImage.css";
-import tillana from "../../../assets/tillana.png"; // adjust path
+import "./slidingImage.css";
+import green_varnum_corner from "../../../assets/green_varnum_corner.png"; // adjust path
 
 function SlidingImage() {
   return (
     <div className="image-wrapper">
-      <img src={tillana} alt="Sliding In" className="slide-in" />
+      <img src={green_varnum_corner} alt="Sliding In" className="slide-in" />
     </div>
   );
 }
