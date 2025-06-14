f9ee3c5f9ab1cf7f437ebf687f0277ba74c1cc91
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-06-14 09:20:12 -0500

    correct the copyright line to be on all pages instead of being in footer

---
+++ web/src/components/copyright/copyright.jsx
@@ -0,0 +1,13 @@
+import "./copyright.css";
+
+const Copyright = () => {
+  return (
+    <div className="copyright">
+      <p className="copyright__text">
+        All Rights Reserved. Copyright {new Date().getFullYear()}
+      </p>
+    </div>
+  );
+};
+
+export default Copyright;
--- web/src/components/footer/footer.jsx
+++ web/src/components/footer/footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./footer.css";
+import Copyright from "../copyright/copyright";
 import { Link, BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import youtubelogo from "../../assets/youtubelogo.png";
 import soorya_studio from "../../assets/soorya_studio.png";
@@ -19,151 +20,154 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <div>
-        <h2 className="Contact_Us"> Contact Us </h2>
-        <div className="box-item">
-          <button className="accordion" onClick={handleClick}>
-            Dance Director
-          </button>
-          <div className="panel">
-            Dance Director
-            <p>
-              Jinoo Varghese <br />
-              (847) 668-0349
-            </p>
-          </div>
-        </div>
-        <div className="box-container">
-          <div>
-            <div className="box-item">
-              <button className="accordion" onClick={handleClick}>
-                Parents of Andrea
-              </button>
-              <div className="panel">
-                Jeril and Shilpa Thomas
-                <p>(708) 548-4971</p>
-              </div>
-            </div>
-            <div className="box-item">
-              <button className="accordion" onClick={handleClick}>
-                Parents of Michelle
-              </button>
-              <div className="panel">
-                Priyan and Jincy Eapen
-                <p>(224) 388-8571</p>
-              </div>
-            </div>
-            <div className="box-item">
-              <button className="accordion" onClick={handleClick}>
-                Parents of Jana
-              </button>
-              <div className="panel">
-                Santosh and Lija Scaria
-                <p>(847) 714-6548</p>
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <div className="box-item">
-              <button className="accordion" onClick={handleClick}>
-                Parents of Rose
-              </button>
-              <div className="panel">
-                Bejoy and Marisa Thomas
-                <p>(847) 722-6976</p>
-              </div>
-            </div>
-            <div className="box-item">
-              <button className="accordion" onClick={handleClick}>
-                Parents of Jenna
-              </button>
-              <div className="panel">
-                John and Leena Plamoottil
-                <p>(847) 219-9914</p>
-              </div>
-            </div>
-            <div className="box-item">
-              <button className="accordion" onClick={handleClick}>
-                Parents of Amarya
-              </button>
-              <div className="panel">
-                John and Latha Chirayil Koola
-                <p>(773) 426-4993</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="box-item">
-          <button className="accordion" onClick={handleClick}>
-            Web Developer
-          </button>
-          <div className="panel">
-            Kanna JJ Plamoottil
-            <p>(847) 338-6855</p>
-          </div>
-        </div>
-
-        <p className="footer__text">
-          All Rights Reserved. Copyright {new Date().getFullYear()}
-        </p>
-        <div className="about__circle"></div>
-        <div className="about__circle about__circle_animation_blurred"></div>
-      </div>
-      <div>
-        <img
-          className="moms_and_daughters"
-          src={moms_daughters}
-          alt="moms__daughters"
-        ></img>
-      </div>
-      <div>
-        <Link to="/video" className="nav-link">
-          Bharatanatyam Journey
-        </Link>
-        <p>Follow us on:</p>
-        <p className="class-location">
-          Soorya Dance School <br />
-          Address: 1948 Raymond Dr <br />
-          Northbrook, IL 60062 <br />
-          Phone: (224) 715-5846
-        </p>
+      <div className="footer__wrapper">
         <div>
-          <div>
-            <a
-              href="https://www.facebook.com/sooryadanceschool"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="facebook-link"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="32"
-                width="32"
-                viewBox="0 0 320 512"
-                fill="#1877F2"
-              >
-                <path d="M279.14 288l14.22-92.66h-88.91V127.89c0-25.35 12.42-50.06 52.24-50.06H293V6.26S273.57 0 256.36 0c-73.22 0-121.06 44.38-121.06 124.72v70.62H89.33V288h46V512h92.66V288z" />
-              </svg>
-            </a>
-            <a
-              href="https://www.youtube.com/@sooryadanceschool3866" // Replace with your actual channel URL
-              target="_blank"
-              rel="noopener noreferrer"
-              className="youtube-link"
-            >
-              <img src={youtubelogo} alt="YouTube" width="30" height="30" />
-            </a>
+          <h2 className="Contact_Us"> Contact Us </h2>
+          <div className="box-item">
+            <button className="accordion" onClick={handleClick}>
+              Dance Director
+            </button>
+            <div className="panel">
+              Dance Director
+              <p>
+                Jinoo Varghese <br />
+                (847) 668-0349
+              </p>
+            </div>
           </div>
+          <div className="box-container">
+            <div>
+              <div className="box-item">
+                <button className="accordion" onClick={handleClick}>
+                  Parents of Andrea
+                </button>
+                <div className="panel">
+                  Jeril and Shilpa Thomas
+                  <p>(708) 548-4971</p>
+                </div>
+              </div>
+              <div className="box-item">
+                <button className="accordion" onClick={handleClick}>
+                  Parents of Michelle
+                </button>
+                <div className="panel">
+                  Priyan and Jincy Eapen
+                  <p>(224) 388-8571</p>
+                </div>
+              </div>
+              <div className="box-item">
+                <button className="accordion" onClick={handleClick}>
+                  Parents of Jana
+                </button>
+                <div className="panel">
+                  Santosh and Lija Scaria
+                  <p>(847) 714-6548</p>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="box-item">
+                <button className="accordion" onClick={handleClick}>
+                  Parents of Rose
+                </button>
+                <div className="panel">
+                  Bejoy and Marisa Thomas
+                  <p>(847) 722-6976</p>
+                </div>
+              </div>
+              <div className="box-item">
+                <button className="accordion" onClick={handleClick}>
+                  Parents of Jenna
+                </button>
+                <div className="panel">
+                  John and Leena Plamoottil
+                  <p>(847) 219-9914</p>
+                </div>
+              </div>
+              <div className="box-item">
+                <button className="accordion" onClick={handleClick}>
+                  Parents of Amarya
+                </button>
+                <div className="panel">
+                  John and Latha Chirayil Koola
+                  <p>(773) 426-4993</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="box-item">
+            <button className="accordion" onClick={handleClick}>
+              Web Developer
+            </button>
+            <div className="panel">
+              Kanna JJ Plamoottil
+              <p>(847) 338-6855</p>
+            </div>
+          </div>
+
+          {/* <p className="footer__text">
+            All Rights Reserved. Copyright {new Date().getFullYear()}
+          </p> */}
+          <div className="about__circle"></div>
+          <div className="about__circle about__circle_animation_blurred"></div>
+        </div>
+        <div>
           <img
-            className="soorya_studio"
-            src={soorya_studio}
-            alt="studio location"
-            width="200"
-            height="200"
+            className="moms_and_daughters"
+            src={moms_daughters}
+            alt="moms__daughters"
           ></img>
         </div>
+        <div>
+          <Link to="/video" className="nav-link">
+            Bharatanatyam Journey
+          </Link>
+          <p>Follow us on:</p>
+          <p className="class-location">
+            Soorya Dance School <br />
+            Address: 1948 Raymond Dr <br />
+            Northbrook, IL 60062 <br />
+            Phone: (224) 715-5846
+          </p>
+          <div>
+            <div>
+              <a
+                href="https://www.facebook.com/sooryadanceschool"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="facebook-link"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="32"
+                  width="32"
+                  viewBox="0 0 320 512"
+                  fill="#1877F2"
+                >
+                  <path d="M279.14 288l14.22-92.66h-88.91V127.89c0-25.35 12.42-50.06 52.24-50.06H293V6.26S273.57 0 256.36 0c-73.22 0-121.06 44.38-121.06 124.72v70.62H89.33V288h46V512h92.66V288z" />
+                </svg>
+              </a>
+              <a
+                href="https://www.youtube.com/@sooryadanceschool3866" // Replace with your actual channel URL
+                target="_blank"
+                rel="noopener noreferrer"
+                className="youtube-link"
+              >
+                <img src={youtubelogo} alt="YouTube" width="30" height="30" />
+              </a>
+            </div>
+            <img
+              className="soorya_studio"
+              src={soorya_studio}
+              alt="studio location"
+              width="200"
+              height="200"
+            ></img>
+          </div>
+        </div>
       </div>
+      <Copyright />
     </footer>
   );
 };
