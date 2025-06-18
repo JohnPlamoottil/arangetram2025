70205892f73787df8d6a30d3efecaf6b88f27b6e
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-06-17 23:23:36 -0500

    initial  set up for the dancers  page using useSearchParams with 1 file with 6dancers

--- web/src/components/pages/dancer/dancer.jsx
+++ web/src/components/pages/dancer/dancer.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 import "./dancer.css"; // optional for styling
 import Navigation from "../../navigation-links/navigation-links";
 
+const dancers = {
+  andrea: { bio: "bio", shishya: "message" },
+};
+
 const Dancer = () => {
+  const [searchParams] = useSearchParams();
+
+  const dancer = searchParams.get("dancer");
+
+  const dancerInfo = dancers[dancer];
   return (
     <div>
       <Navigation />
+      <h1>{dancerInfo.shishya}</h1>
+      <h3>{dancerInfo.bio}</h3>
     </div>
   );
 };
