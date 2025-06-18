b2fb023a338afd4786deea39c4cc174e3df8660f
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-06-18 14:17:36 -0500

    add dancer name option to message js in server

--- server/messages.js
+++ server/messages.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
+  dancerName: { type: String, required: true },
   name: { type: String, required: true },
   content: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
