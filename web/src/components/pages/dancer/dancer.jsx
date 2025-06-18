3a52214297a2528fc262b6f04501e2a1e5d0d4fa
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-06-18 06:41:20 -0500

    initial setup to put in template for accordion dropdown for dancers, Michelle is first and first two style in dances css is title for std and 430px

--- web/src/components/pages/dancer/dancer.jsx
+++ web/src/components/pages/dancer/dancer.jsx
@@ -3,8 +3,8 @@ import { useSearchParams } from "react-router-dom";
 import "./dancer.css"; // optional for styling
 import Navigation from "../../navigation-links/navigation-links";
 
-const dancers = {
-  andrea: { bio: "bio", shishya: "message" },
+const Dancers = {
+  Michelle: { bio: "bio", shishya: "message" },
 };
 
 const Dancer = () => {
@@ -12,10 +12,64 @@ const Dancer = () => {
 
   const dancer = searchParams.get("dancer");
 
-  const dancerInfo = dancers[dancer];
+  const dancerInfo = Dancers[dancer];
+
+  function handleClick(e) {
+    const button = e.target;
+    button.classList.toggle("slide");
+    const panel = button.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
   return (
     <div>
       <Navigation />
+      <section className="questions">
+        <h2 className="title_Andrea">Michele Eapen</h2>
+        <button className="accordion" onClick={handleClick}>
+          ..
+        </button>
+        <div className="panel">
+          <p>
+            ..
+            <br />
+            ..
+          </p>
+        </div>
+        <button className="accordion" onClick={handleClick}>
+          ..
+        </button>
+        <div className="panel">
+          <p className="">
+            ..
+            <img className="" alt="auditorium" />
+          </p>
+        </div>
+        <button className="accordion" onClick={handleClick}>
+          ..
+        </button>
+        <div className="panel">
+          <p>..</p>
+          <p>..</p>
+          <p>..</p>
+        </div>
+        <button className="accordion" onClick={handleClick}>
+          ..
+        </button>
+        <div className="">
+          <p className="">..</p>
+          <div className=""></div>
+        </div>
+        <button className="accordion" onClick={handleClick}>
+          ..
+        </button>
+        <div className="panel">
+          <p>..</p>
+        </div>
+      </section>
       <h1>{dancerInfo.shishya}</h1>
       <h3>{dancerInfo.bio}</h3>
     </div>
@@ -23,3 +77,48 @@ const Dancer = () => {
 };
 
 export default Dancer;
+
+// copy paste section for the next dancer:
+// <section className="questions">
+//   <h2 className="title_Andrea">Michele Eapen</h2>
+//   <button className="accordion" onClick={handleClick}>
+//     ..
+//   </button>
+//   <div className="panel">
+//     <p>
+//       ..
+//       <br />
+//       ..
+//     </p>
+//   </div>
+//   <button className="accordion" onClick={handleClick}>
+//     ..
+//   </button>
+//   <div className="panel">
+//     <p className="">
+//       ..
+//       <img className="" alt="auditorium" />
+//     </p>
+//   </div>
+//   <button className="accordion" onClick={handleClick}>
+//     ..
+//   </button>
+//   <div className="panel">
+//     <p>..</p>
+//     <p>..</p>
+//     <p>..</p>
+//   </div>
+//   <button className="accordion" onClick={handleClick}>
+//     ..
+//   </button>
+//   <div className="">
+//     <p className="">..</p>
+//     <div className=""></div>
+//   </div>
+//   <button className="accordion" onClick={handleClick}>
+//     ..
+//   </button>
+//   <div className="panel">
+//     <p>..</p>
+//   </div>
+// </section>
