9d3e5c04cd21c0bde3949da7585ce31fdc46050a
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-06-18 05:00:59 -0500

    edit program with MC names and delete any duplicates

--- web/src/components/pages/program/program.jsx
+++ web/src/components/pages/program/program.jsx
@@ -32,8 +32,10 @@ const Program = () => {
           </button>
           <div className="panel_program">
             <p>
-              Good evening everyone. I am MC1, one of the MCs of the evening. In
-              5 minutes, we will be starting the 'Samarpanam 2025' function.
+              Good evening everyone. I am Brianna Chirayil (Megha), one of the
+              MCs for this evening and I am Jasmine Vayalil, the other MC for
+              this evening. <br />
+              In 5 minutes, we will be starting the 'Samarpanam 2025' function.
               Everyone please take your seats now so we can start the program at
               4:00pm. The doors will be closed at 4pm and we request that
               everyone be seated during the program to avoid any disruption. We
@@ -41,34 +43,22 @@ const Program = () => {
               be a disruption to the program. Coffee will be available in the
               Hallway by 5pm. Please be sure to grab a brochure on your way in.
             </p>
-            <p>
-              Good evening everyone. I am MC2, one of the MCs of the evening. In
-              5 minutes, we will be starting the 'Samarpanam 2025' function.
-              Everyone please take your seats now so we can start the program at
-              4:00pm. The doors will be closed at 4pm and we request that
-              everyone be seated during the program to avoid any disruption. We
-              also ask that you please silence your cell phones as it will also
-              be a disruption to the program. Please move towards the center of
-              the auditorium so that those who are coming late can quikly be
-              seated. Coffee will be available in the Hallway by 5pm. Please be
-              sure to grab a brochure on your way in.
-            </p>
           </div>
           <button className="accordion_program" onClick={handleClick}>
-            Welcome Message - Promptly Begins @ 4:00pm
+            Welcome Message - Starts @ 4:00pm
           </button>
           <div className="panel_program">
             <p>
               Respected Priests, Invited Guests, Family and Friends, I welcome
-              you all to the eagerly awaited "SAMARPANAM 2025" Bharathanatyam
+              you all to the eagerly awaited "Samarpanam 2025" Bharathanatyam
               Arangetram of 6 talented young dancers - Amarya Koola, Andrea
               Jeril Thomas, Jana Scaria, Jenna Plamoottil, Michele Eapen & Rose
               Thomas - which is being held under the guidance of Smt. Jinoo
-              Varghese, the artistic director of Soorya Dance School. These
-              young girls have worked very hard to achieve their dreams and this
-              was possible only because of the love and unwavering support of
-              their parents. <br />I would like to invite the parents of the
-              dancers to the stage ... <br />
+              Varghese, the artistic director of Soorya Dance School. <br />
+              These young girls have worked very hard to achieve their dreams
+              and this was possible only because of the love and unwavering
+              support of their parents. <br />I would like to invite the parents
+              of the dancers to the stage ... <br />
               the parents of Andrea - Jeril and Shilpa Thomas , <br />
               the parents of Jana - Santosh and Lija Scaria , <br />
               the parents of Michelle - Priyan & Jincy Eapen , <br />
@@ -77,10 +67,10 @@ const Program = () => {
               the parents of Amarya - John and Latha Chirayil Koola <br />
               Thank you to all of the parents for all of your hardwork. For any
               function, the presence of priests is important for God's blessing.
-              I respectfully invite Rev. Fr. - , to the stage to bless this
-              occasion. I would like to invite Rev. Fr. - to offer a short
-              prayer. I would like to invite Reverend Vicar for the welcome
-              address on behalf of the parents.
+              I respectfully invite Rev. Fr.Jerry Mathew, to the stage to bless
+              this occasion. I would like to invite Rev. Fr.Jerry to offer a
+              short prayer. I would like to invite Reverend Vicar Jerry Mathew
+              for the welcome address on behalf of the parents.
             </p>
           </div>
           <button className="accordion_program" onClick={handleClick}>
@@ -138,14 +128,14 @@ const Program = () => {
               The musicians for the evening are: <br />
               Nattuvangam - Smt. Jinoo Varghese (Guru/Instructor)
               <br />
-              <span className="program__Musicians">
-                Vocals - Sri.Deepu Karunakaran{" "}
-              </span>
-              <br />
+              Vocals - Sri.Deepu Karunakaran <br />
               Mridangam - Sri Kiran R. Pai <br />
               Violin - Sri Rijesh Gopalakrishnan <br />
               Flute - Sri Preyesh Mampoyil Kudiru Parambu <br />
-              Please refer to the Musician Page to view each of their biography.
+              <p className="refer">
+                Please refer to the Musician Page to view each of their
+                biography.
+              </p>
             </p>
             <p>
               Lets welcome the dancers to introduce their first item of this
@@ -191,12 +181,13 @@ const Program = () => {
           </button>
           <div className="panel_program">
             <p>
-              [MC1] Next will be a solo presentation by Andrea Jerril Thomas.{" "}
+              [Megha] Next will be a solo presentation by Andrea Jerril Thomas.{" "}
               <br />
-              First, I would like to invite onto the stage to say a few words.
+              First, I would like to invite .... onto the stage to say a few
+              words.
               <br />
               Let us watch Andrea's solo performance. <br />
-              [MC2] - Next item is ‘Sada shiva Kavuthuvam’, Kauthuvams are
+              [Jasmine] - Next item is ‘Sada shiva Kavuthuvam’, Kauthuvams are
               traditional invocatory pieces performed in praise of a deity,
               blending rhythmic syllables with expressive storytelling. In this
               item, the dancer offers reverence to Lord Sadashiva, the eternal
@@ -225,17 +216,18 @@ const Program = () => {
           </button>
           <div className="panel_program">
             <p>
-              [MC1] Next will be a solo presentation by Jana Scaria. First, I
-              would like to invite onto the stage to say a few words. [ speaks]
-              Let us watch Jana's solo performance. <br />
-              [MC(2)] ‘Esther’ is a Padam based on the biblical story of Esther,
-              the beautiful Jewish queen. The dancer describes the story where
-              Esther asks the king to a dinner party. The king was happy and
-              granted her a wish, Esther reveals her true purpose and explains
-              Haman's wicked plan to destroy her and her people. The King
-              ordered to execute Haman on the gallows he built for Mordecai. The
-              Story of Esther emphasizes the power of God, instructing us we
-              should use his blessings to help others. Orchestra plays Ragam
+              [Megha] Next will be a solo presentation by Jana Scaria. First, I
+              would like to invite .... onto the stage to say a few words. Let
+              us watch Jana's solo performance. <br />
+              [Jasmine] ‘Esther’ is a Padam based on the biblical story of
+              Esther, the beautiful Jewish queen. The dancer describes the story
+              where Esther asks the king to a dinner party. The king was happy
+              and granted her a wish, Esther reveals her true purpose and
+              explains Haman's wicked plan to destroy her and her people. The
+              King ordered to execute Haman on the gallows he built for
+              Mordecai. The Story of Esther emphasizes the power of God,
+              instructing us we should use his blessings to help others.
+              Orchestra plays Ragam
             </p>
             <p>
               Ragam: Ragamalika <br />
@@ -255,18 +247,18 @@ const Program = () => {
           </button>
           <div className="panel_program">
             <p>
-              [MC1] Next will be a solo presentation by Michelle Eapen. First, I
-              would like to invite onto the stage to say a few words. [ speak]
-              Let us watch Michelle's solo performance. <br />
-              [MC2] - ‘Durge Durge Jaya Jaya Durge’ appears to be a devotional
-              phrase in Sanskrit dedicated to the goddess Durga, a Hindu deity
-              symbolizing strength and protection. The words “Durge Durge”
-              invoke the name of Durga twice, emphasizing her power. “Jaya Jaya”
-              translates to “victory, victory,” praising her triumph. This
-              phrase is commonly chanted during festivals like Navaratri, where
-              Durga is celebrated for her victory over evil. The repetition of
-              her name and the phrase’s meaning emphasize devotion and
-              reverence. Orchestra plays Ragam
+              [Megha] Next will be a solo presentation by Michelle Eapen. First,
+              I would like to invite .... onto the stage to say a few words. Let
+              us watch Michelle's solo performance. <br />
+              [Jasmine] - ‘Durge Durge Jaya Jaya Durge’ appears to be a
+              devotional phrase in Sanskrit dedicated to the goddess Durga, a
+              Hindu deity symbolizing strength and protection. The words “Durge
+              Durge” invoke the name of Durga twice, emphasizing her power.
+              “Jaya Jaya” translates to “victory, victory,” praising her
+              triumph. This phrase is commonly chanted during festivals like
+              Navaratri, where Durga is celebrated for her victory over evil.
+              The repetition of her name and the phrase’s meaning emphasize
+              devotion and reverence. Orchestra plays Ragam
             </p>
             <p>
               Ragam: Revathi <br />
@@ -286,12 +278,12 @@ const Program = () => {
           </button>
           <div className="panel_program">
             <p>
-              [MC1] This Varnam is a tribute to Kerala — a land shaped by
+              [Megha] This Varnam is a tribute to Kerala — a land shaped by
               legend, where sage Parasurama is believed to have reclaimed the
               earth from the sea. Through graceful movements and expressive
               storytelling, the dancer explores Kerala’s natural beauty,
               spiritual harmony, and rich cultural heritage. <br />
-              [MC2]The piece celebrates the state’s classical art forms,
+              [Jasmine]The piece celebrates the state’s classical art forms,
               including the dramatic elegance of Kathakali and the gentle grace
               of Mohiniyattam, capturing the vibrant soul of God’s Own Country.{" "}
               <br />
@@ -312,12 +304,12 @@ const Program = () => {
           </button>
           <div className="panel_program">
             <p>
-              MC1 announces coffee break <br />
+              Megha announces coffee break <br />
               We will now take a 10 minute coffee break. Coffee is available
               outside of the theater. Please do not bring food or drink into the
               auditorium. Please be back by in the auditorium by 6:10. Thank
               you. <br />
-              MC2 announces short intermission <br />
+              Jasmine announces short intermission <br />
               We are now half way done with our program. I request everyone to
               be seated and to not exit while the program is still going on
               (unless it is an emergency ). Continuous movement will cause much
@@ -336,9 +328,10 @@ const Program = () => {
           </button>
           <div className="panel_program">
             <p>
-              [MC1] Next will be a solo presentation by Rose Thomas. First, I
-              would like to invite [] onto the stage to say a few words. <br />
-              Let us watch Rose's solo performance. <br />
+              [Megha] Next will be a solo presentation by Rose Thomas. First, I
+              would like to invite .... onto the stage to say a few words.{" "}
+              <br />
+              [Jasmine] Let us watch Rose's solo performance. <br />
               Pradosha Samayadi’ is a vibrant composition that describes the
               dance of Lord Shiva during Pradosha Samaya (bimonthly occasion on
               the thirteenth day of every fortnight in the Hindu calendar).The
@@ -364,10 +357,12 @@ const Program = () => {
           </button>
           <div className="panel_program">
             <p>
-              [MC1] Next will be a solo presentation by Jenna Plamoottil. <br />
-              First, I would like to invite onto the stage to say a few words. [
-              speak] <br />
-              Let us watch Jenna's solo performance. <br />
+              [Megha] Next will be a solo presentation by Jenna Plamoottil.{" "}
+              <br />
+              First, I would like to invite .... onto the stage to say a few
+              words.
+              <br />
+              [Jasmine] Let us watch Jenna's solo performance. <br />
               ‘Magdalena Mariam’, is based on a renowned Malayalam poem by
               Mahakavi Vallathol Narayana Menon. It portrays a powerful moment
               from Mary Magdalene’s life — from being condemned as a sinner to
@@ -395,10 +390,11 @@ const Program = () => {
           </button>
           <div className="panel_program">
             <p>
-              [MC1] Next will be a solo presentation by Amarya Koola. <br />
-              First, I would like to invite onto the stage to say a few words. [
-              speak] <br />
-              Let us watch Amarya's solo performance. <br />
+              [Jasmine] Next will be a solo presentation by Amarya Koola. <br />
+              First, I would like to invite .... onto the stage to say a few
+              words.
+              <br />
+              [Megha] Let us watch Amarya's solo performance. <br />
               ‘Tarangam’ means waves and features stories from Lord Krishna’s
               life — his childhood, playful pranks, and marriage to Rukmini.
               This piece highlights Krishna’s divine beauty and the deep love of
@@ -422,8 +418,9 @@ const Program = () => {
           </button>
           <div className="panel_program">
             <p>
-              [MC1] Next is a musical interlude <br />
-              [MC2] We have come to the last item of today's performance. <br />
+              [Megha] Next is a musical interlude <br />
+              [Jasmine] We have come to the last item of today's performance.{" "}
+              <br />
               Let's hear the details of this item directly from the dancers. To
               conclude today’s arangetram recital we will be presenting
               Thillana. ‘Thillana’, is a vibrant finale to a Bharatanatyam
@@ -454,13 +451,13 @@ const Program = () => {
           </button>
           <div className="panel_program">
             <p>
-              [MC] I would like to invite the Guru of the artists and the
+              [Jasmine] I would like to invite the Guru of the artists and the
               Artistic Director of Soorya Dance School, Smt. Jinoo Varghese, to
               the stage <br />
               [Jinoo] Vote of Thanks, Gift presentations (musicans) and plaque
               presentations to the dancers. Gift presentations.... <br />
-              [MC1] Thank you all for being such a great audience. We would like
-              to extend our Thanks to the College of Lake County for this
+              [Megha] Thank you all for being such a great audience. We would
+              like to extend our Thanks to the College of Lake County for this
               excellent theatre and a special thanks to the team for their
               excellent support. Thank you, and have a good night!
             </p>
